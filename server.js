cf7775452022b90c585da7cfb4058f479abfe5ae
unknown <youssef.ojeil@hotmail.com>
2022-11-02 16:42:38 -0400

    import mysql2 & setup db connection

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+// import express & mysql2
 const express = require("express");
+const mysql = require("mysql2");
 
 // set up port
 const PORT = process.env.PORT || 3000;
-
 // invoke express
 const app = express();
 
-// setup middleware
+// Setup Express Middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      password: 'rootroot',
+      database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
+  );
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
