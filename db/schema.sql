85515f2cfaa85575b5461b6386d09cba0fdf18bc
unknown <youssef.ojeil@hotmail.com>
2022-11-01 23:49:40 -0400

    create basic schema

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+--delete db if exists & create db--
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+-- target company database
+USE company_db;
+
+-- Department Table
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+)
+
+-- Role Table
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+
+)
+
+-- Employee Table
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+    PRIMARY KEY (id)
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+)
